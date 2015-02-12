4dfeff2fdc9df133ff35da606f40a39be8e35d12
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-12 06:14:18 -0500

    Files made and Dog Years started
    first commit for expression worksheet

---
+++ Expressions_worksheet/js/expressions.js
@@ -0,0 +1,14 @@
+// Deaven Brown
+// February 11 2015
+// Expressions Worksheet
+
+// Add add alert to test
+//alert("Javascript works!");
+
+//Dog Years
+//Give Sparky an age
+var actualAge = 4;
+//Multiply Sparky's age by 7 to have his age in "dog years"
+var dogYears = actualAge * 7;
+//Show Sparky's age in console
+console.log("Sparky is " + actualAge + " human years old which is " + dogYears + " in dog years.");
