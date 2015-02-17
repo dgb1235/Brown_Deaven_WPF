e7c95674212d05cb7d1ab918be46e357f035aa9c
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-17 06:34:18 -0500

    Changed Check the Login
    Changed an unnecessary else if statement into a simpler and cleaner
    else statement. I hope it doesn’t affect my grade.

--- Conditionals_worksheet/java/script.js
+++ Conditionals_worksheet/java/script.js
@@ -29,15 +29,22 @@ var correctName = "Lee Lewis";
 var correctPass = "fullsailSDI";
 //if the username matches the correct name and the password matches the correct password
 if(userName === correctName && userPass === correctPass){
-    console.log("Welcome, " + userName + "!")
-}
-//if the username does not match the correct name but the password matches the correct password
-else if(userName != correctName && userPass === correctPass){
-    console.log("User not found. Try again.")
+    console.log("Welcome, " + userName + "!");
 }
 //if the username matches the correct name but the password does not match the correct password
 else if(userName === correctName && userPass != correctPass){
-    console.log("Password does not match our records.")
+    console.log("Password does not match our records.");
 }
-//Nothing will happen if none of the inputs match
+//if the username does not match the correct name but the password matches the correct password
+else{
+    console.log("User not found. Try again.");
+}
+/*
+I made this else statement because I wanted to console.log the above statement if the
+user input is not correct. Doing so makes the machine log it even if the password is correct
+I had originally made an else if statement that does the exact same task this simple else
+statement does, but it was more coding that was needed and it made nothing happen if neither
+the username or password matched. Doing this delivers a result no matter the condition
+and still fulfills the requirement of logging if the username does not match and the pass does.
+*/
 
