8ab153fda927a900706d7da7970119091151e2aa
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-17 06:21:42 -0500

    Check the Login!
    Started and completed the check the login assignment. Would’ve done it
    sooner, but I forgot that && was the code for and.

--- Conditionals_worksheet/java/script.js
+++ Conditionals_worksheet/java/script.js
@@ -14,7 +14,30 @@ var milesLeft = gallonsLeft * gasEfficiency;
 if(milesLeft >= 200){
     console.log("Yes, you can make it without stopping for gas");
 }
-//if the amount of miles left is les than 200
+//if the amount of miles left is less than 200
 else{
     console.log("You only have " + gallonsLeft + " gallons of gas in your tank, better get gas now while you can!");
 }
+
+//Check the Login!
+//Givens
+//Make prompts for the user to input username and password
+var userName = prompt("Please enter your username.");
+var userPass = prompt("Please enter your password.");
+//Make prompts storing the correct username and password
+var correctName = "Lee Lewis";
+var correctPass = "fullsailSDI";
+//if the username matches the correct name and the password matches the correct password
+if(userName === correctName && userPass === correctPass){
+    console.log("Welcome, " + userName + "!")
+}
+//if the username does not match the correct name but the password matches the correct password
+else if(userName != correctName && userPass === correctPass){
+    console.log("User not found. Try again.")
+}
+//if the username matches the correct name but the password does not match the correct password
+else if(userName === correctName && userPass != correctPass){
+    console.log("Password does not match our records.")
+}
+//Nothing will happen if none of the inputs match
+
