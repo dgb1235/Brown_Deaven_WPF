397db6ac32fc7014aa591cee1df980dab073fb55
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-19 22:42:47 -0500

    Fixed repetitions
    Repetitive strings were taken out

--- Brown_Deaven_Conditionals_Assignment/java/script.js
+++ Brown_Deaven_Conditionals_Assignment/java/script.js
@@ -13,20 +13,20 @@ var savingsPercent = percentToSavings / 100;
 var leftOver = userNetIncome - userBills;
 var intoSavings = leftOver * savingsPercent;
 var yearlyInterest = intoSavings * 12 * savingsInterest;
-var madeInSavings =  yearlyInterest / 12;
+var monthlyInterest =  yearlyInterest / 12;
 //If your net monthly income is greater than or equal to 2 times your monthly bills
-(userNetIncome >= 2 * userBills && userSavings === "yes") ? console.log("Wise choice because you make $" + madeInSavings + " per month and " + yearlyInterest + " per year in your savings account.") : console.log("You're missing out on a free $" + madeInSavings + " per month, just for having a savings account and putting " + percentToSavings + "% in every month. That's " + yearlyInterest + " per year.");
+(userNetIncome >= 2 * userBills && userSavings === "yes") ? console.log("Wise choice because you make $" + monthlyInterest + " per month and " + yearlyInterest + " per year in your savings account.") : console.log("You're missing out on a free $" + monthlyInterest + " per month, just for having a savings account and putting " + percentToSavings + "% in every month. That's " + yearlyInterest + " per year.");
 //If your savings account will make you more than 25 dollars per month
-if(madeInSavings > 25){
-    console.log("You should definitely have a savings account. That's an extra $" + madeInSavings + " per month and $" + yearlyInterest + " per year");
+if(monthlyInterest > 25){
+    console.log("You should definitely have a savings account.");
 }
-//If your savings account will make more than 5 dollars per month
-else if(madeInSavings > 5){
-    console.log("Having a savings account can't hurt you. It's an extra $" + madeInSavings + " per month. and $" + yearlyInterest + " per year.");
+//If your savings account will make more than 1 dollars per month
+else if(monthlyInterest > 1){
+    console.log("Having a savings account can't hurt you.");
 }
-//If your savings account will make less than 5 dollars per month
+//If your savings account will make less than 1 dollars per month
 else{
-    console.log("You don't really need a savings account, it's only an extra $" + madeInSavings + " per month or $" + yearlyInterest + "per year, but if you have or start one then more power to you.");
+    console.log("You don't really need a savings account.");
 }
 //How the user can make a savings account if they do not already have one
 if(userSavings === "" || "no"){
