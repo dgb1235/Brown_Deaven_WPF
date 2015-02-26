aca858741f88d052706b7ce462e94e279357c3a3
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-26 00:46:28 -0500

    Finished Circumference
    That didn’t take long at all

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,3 +2,12 @@
 //February 26, 2015
 //Functions Worksheet
 
+//Circumference
+var radius = 12;
+var circumference = calculate(radius);
+
+function calculate(radius){
+    return 3.14 * (2 * radius);
+}
+
+console.log("If the radius of a circle is " + radius + " inches, the circumference of the circle is " + circumference + " inches.");
