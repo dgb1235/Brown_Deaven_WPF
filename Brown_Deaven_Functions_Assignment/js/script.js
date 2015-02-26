6fa339d8d04249bffe5d6512680f8ade0841bc9c
dgb1235 <deavenbrown@Deavens-MacBook-Pro.local>
2015-02-26 02:48:13 -0500

    Changes
    Made it so the code would not allow the winning PowerBall number to be
    greater than 35 by creating a new function specifically for the
    PowerBall number.

--- Brown_Deaven_Functions_Assignment/js/script.js
+++ Brown_Deaven_Functions_Assignment/js/script.js
@@ -3,16 +3,20 @@
 //Functions Assignment
 
 //Declare variables
-var numberOne = number(1);
-var numberTwo = number(1);
-var numberThree = number(1);
-var numberFour = number(1);
-var numberFive = number(1);
-var powerBall = number(1);
+var numberOne = number();
+var numberTwo = number();
+var numberThree = number();
+var numberFour = number();
+var numberFive = number();
+var powerBall = power();
 //Declare functions
-function number(para){
-    var variable =  para * Math.round(Math.random() * 59);
-    return variable;
+function number(){
+    var random = Math.round(Math.random() * 59);
+    return random;
+}
+function power(){
+    var random = Math.round(Math.random() * 35);
+    return random
 }
 //Log outcome
 console.log("Your winning numbers for tonight's PowerBall are " + numberOne + ", " + numberTwo + ", " + numberThree + ", " + numberFour + ", " + numberFive + ", and your winning PowerBall number is " + ", " + powerBall + ". Thank you for playing!")
